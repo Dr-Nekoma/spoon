fc666b09ee8f36746eec6a9bcabb744632a0384d
Vinícius Müller <vinigm.nho@gmail.com>
2022-08-02 22:48:34 -0300

    feat: add nix flakes

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "spoon - the compiler for the silverware language";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    { } // utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            rebar3
+            erlang
+            erlang-ls
+          ];
+        };
+      });
+}
