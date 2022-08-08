c08e1b9d1e6c7fa45cc9d2d06bbb5464fa9a1b2f
Marcos Magueta <maguetamarcos@gmail.com>
2022-08-08 00:19:23 -0300

    add variadic abstractions and native arithmetic
    Co-authored-by: Nathanael Ribeiro <ribeirotomas1904@gmail.com>
    Co-authored-by: Eduardo Lemos <dudulr10@gmail.com>

--- src/spoon.erl
+++ src/spoon.erl
@@ -2,7 +2,7 @@
 
 -import(do, [fmap/2]).
 
--export([eval/0, prelude/0, fmap_maybe/0, increment/1]).
+-export([eval/0]).
 
 %% (fun x -> x(x)) (fun x -> x(x))
 %% {application,
@@ -13,10 +13,10 @@
 
 evalWithEnvironment(_, {literal, _} = Expression) ->
     {ok, Expression};
-evalWithEnvironment(Environment, {abstraction, Label, Body}) ->
-    {ok, {closure, Label, Body, Environment}};
-evalWithEnvironment(Environment, {application, Abstraction, Value}) ->
-    evalApplication(Environment, Abstraction, Value);
+evalWithEnvironment(Environment, {abstraction, Parameters, Body}) ->
+    {ok, {closure, Parameters, Body, Environment}};
+evalWithEnvironment(Environment, {application, Abstraction, Arguments}) ->
+    evalApplication(Environment, Abstraction, Arguments);
 evalWithEnvironment(Environment, {variable, Name}) ->
     case maps:find(Name, Environment) of
         error ->
@@ -25,19 +25,101 @@ evalWithEnvironment(Environment, {variable, Name}) ->
             Result
     end.
 
-evalClosure(ClosedEnvironment, Environment, Value, Label, Body) ->
-    do:do(evalWithEnvironment(Environment, Value), [ fun (EvaluatedValue) -> NewEnvironment = maps:put(Label, EvaluatedValue, ClosedEnvironment),
-									     evalWithEnvironment(NewEnvironment, Body) end ]).
+%% (defun fun (a b &rest c) ())
+%% (fun 1 2 3 4 5 6 7)
+%% Arguments = [{literal, 1}, {variable, x}, {literal, 2}, {variable, y}, {literal, 4}]
+%% Parameters = {isVariadic, [{argument, a}, {argument, b}, {rest, c}]}
+%% Check if the arguments and variadics conform with the function annotation
+%% Evaluate arguments into a list
+%% Zip arguments with labels
+%% Add variatic argument to the environment as a list, necessarily
+evalClosure(ClosedEnvironment, Environment, Arguments, Parameters, Body) ->
+    case Parameters of
+        {variadic, Labels} ->
+            if length(Arguments) >= length(Labels) ->
+                   EvaluatedValues =
+                       lists:map(fun(X) -> evalWithEnvironment(Environment, X) end, Arguments),
+                   FindAnError =
+                       lists:search(fun ({error, _}) ->
+                                            true;
+                                        (_) ->
+                                            false
+                                    end,
+                                    EvaluatedValues),
+                   case FindAnError of
+                       {value, Error} ->
+                           Error;
+                       false ->
+                           {NonVariadicArgs, VariadicArgs} =
+                               lists:split(length(Labels) - 1, EvaluatedValues),
+                           {NonVariadicNames, [{rest, VariadicName}]} =
+                               lists:split(length(Labels) - 1, Labels),
+                           ParamEnv =
+                               maps:from_list(
+                                   lists:merge(
+                                       lists:zip(
+                                           lists:map(fun({argument, X}) -> X end, NonVariadicNames),
+                                           lists:map(fun({ok, X}) -> X end, NonVariadicArgs)),
+                                       [{VariadicName,
+                                         lists:map(fun({ok, X}) -> X end, VariadicArgs)}])),
 
-evalNativeFunction(Environment, Function, Value) ->
-    do:do(evalWithEnvironment(Environment, Value), [Function]).
+                           NewEnvironment = maps:merge(ParamEnv, ClosedEnvironment),
+                           evalWithEnvironment(NewEnvironment, Body)
+                   end
+            end;
+        {notVariadic, Labels} ->
+            EvaluatedValues =
+                lists:map(fun(X) -> evalWithEnvironment(Environment, X) end, Arguments),
+            io:fwrite("A: ~p~n", EvaluatedValues),
+            FindAnError =
+                lists:search(fun ({error, _}) ->
+                                     true;
+                                 (_) ->
+                                     false
+                             end,
+                             EvaluatedValues),
+            io:fwrite("B: ~p~n", [FindAnError]),
+            case FindAnError of
+                {value, Error} ->
+                    Error;
+                false ->
+                    ParamEnv =
+                        maps:from_list(
+                            lists:zip(
+                                lists:map(fun({argument, X}) -> X end, Labels),
+                                lists:map(fun({ok, X}) -> X end, EvaluatedValues))),
+                    io:fwrite("C: ~p~n", [ParamEnv]),
+                    NewEnvironment = maps:merge(ParamEnv, ClosedEnvironment),
+                    evalWithEnvironment(NewEnvironment, Body)
+            end
+    end.
 
-evalApplication(Environment, Abstraction, Value) ->
+prelude() ->
+    maps:from_list([add(), subtract(), multiplication(), division()]).
+
+evalNativeFunction(Environment, Function, Arguments) ->
+    EvaluatedValues = lists:map(fun(X) -> evalWithEnvironment(Environment, X) end, Arguments),
+    FindAnError =
+        lists:search(fun ({error, _}) ->
+                             true;
+                         (_) ->
+                             false
+                     end,
+                     EvaluatedValues),
+
+    case FindAnError of
+        {value, Error} ->
+            Error;
+        false ->
+            Function(EvaluatedValues)
+    end.
+
+evalApplication(Environment, Abstraction, Arguments) ->
     case evalWithEnvironment(Environment, Abstraction) of
-        {ok, {closure, Label, Body, ClosedEnvironment}} ->
-            evalClosure(ClosedEnvironment, Environment, Value, Label, Body);
+        {ok, {closure, Parameters, Body, ClosedEnvironment}} ->
+            evalClosure(ClosedEnvironment, Environment, Arguments, Parameters, Body);
         {ok, {nativeFunc, Function}} ->
-            evalNativeFunction(Environment, Function, Value);
+            evalNativeFunction(Environment, Function, Arguments);
         _ ->
             {error,
              string:concat("Could not apply the following value, because it is not a function: ",
@@ -45,17 +127,20 @@ evalApplication(Environment, Abstraction, Value) ->
     end.
 
 genNativeFunc(Operator) ->
+    Function =
+        fun(Element, Accum) ->
+           case {Accum, Element} of
+               {{ok, {literal, Acc}}, {ok, {literal, X}}} ->
+                   {ok, {literal, Operator(Acc, X)}};
+               _ ->
+                   {error, "Could not operate on this"}
+           end
+        end,
     {nativeFunc,
-     fun ({literal, X}) ->
-             {ok,
-              {nativeFunc,
-               fun ({literal, Y}) ->
-                       {ok, {literal, Operator(X, Y)}};
-                   (_) ->
-                       {error, "A"}
-               end}};
-         (_) ->
-             {error, "B"}
+     fun(List) ->
+        [Head | Tail] = List,
+        io:fwrite("~p~n", [Tail]),
+        lists:foldl(Function, Head, Tail)
      end}.
 
 add() ->
@@ -75,35 +160,18 @@ division() ->
                            A / B
                    end)}.
 
-prelude() ->
-    maps:from_list([add(), subtract(), multiplication(), division()]).
-
 eval() ->
-    Expression = {application, {application, {variable, opAdd}, {literal, 40}}, {abstraction, y, {variable, y}}},
-    %% Expression = {application, {application, {variable, opAdd}, {literal, 40}}, {literal, 2}},
-    evalWithEnvironment(prelude(), Expression).
-
-increment(N) ->
-  N + 1.
-
-fmap_maybe() ->
-  fmap(fun increment/1, {just, 1}).
-
-% [+ 1 2 3 4 5]
-
-% (defun abc (x:string, y: string, z: &rest int))
-% (abc "A" "B" 1 2 3 4 5 6 7)
-
-% {node: abstraction, parameter: [x, y, z:[]], parameter_type: [int, int, int[]], body: {node: variable, label: x} }
-
-% [1, 2, ["asd", "xyz"]]
-
-% [int, int, int]
-% [1, 2, 3]
-
-% lists:all(fun (Type, {Type2, _}) -> Type == typeOf(Type2) , Arg2)
-% [(t_integer, {integer, 1}), (int, 2), (int, 3)]
-
-% integer -> t_integer
-
-% typeOf env expression -> type
+    %%Expression =
+    %%    {application,
+    %%     {abstraction, {notVariadic, [{argument, y}]}, {variable, y}},
+    %%     [{literal, 1}]},
+    %%Expression =
+    %%    {application,
+    %%    {abstraction, {variadic, [{rest, b}]}, {variable, b}},
+    %%     [{literal, 1}, {literal, 2}, {literal, 3}]},
+    Expression =
+        {application,
+         {variable, opDiv},
+         [{literal, 1}, {literal, 2}, {literal, 2}, {literal, 2}]},
+    evalWithEnvironment(prelude(),
+                        Expression).    %%evalWithEnvironment(maps:new(), Expression).
