9778bc9ddf15c16bbe1d047181d7c4263d30e418
Eduardo Lemos Rocha <dudulr10@gmail.com>
2022-08-12 21:15:15 -0300

    Added conditions and boolean operators

--- src/spoon.erl
+++ src/spoon.erl
@@ -13,6 +13,8 @@
 
 evalWithEnvironment(_, {literal, _} = Expression) ->
     {ok, Expression};
+evalWithEnvironment(Environment, {condition, Case, Then, Else}) ->
+    evalCondition(Environment, Case, Then, Else);
 evalWithEnvironment(Environment, {abstraction, Parameters, Body}) ->
     {ok, {closure, Parameters, Body, Environment}};
 evalWithEnvironment(Environment, {application, Abstraction, Arguments}) ->
@@ -23,6 +25,14 @@ evalWithEnvironment(Environment, {variable, Name}) ->
             {error, string:concat("Unbound variable: ", Name)};
         Result ->
             Result
+    end.
+
+
+evalCondition(Environment, Case, Then, Else) ->
+    case evalWithEnvironment(Environment, Case) of
+        {ok, {literal, {boolean, true}}} -> evalWithEnvironment(Environment, Then);
+        {ok, {literal, {boolean, false}}} -> evalWithEnvironment(Environment, Else);
+        _ -> erlang:error("Expression could not be reduced to boolean")
     end.
 
 %% (defun fun (a b &rest c) ())
@@ -94,9 +104,6 @@ evalClosure(ClosedEnvironment, Environment, Arguments, Parameters, Body) ->
             end
     end.
 
-prelude() ->
-    maps:from_list([add(), subtract(), multiplication(), division()]).
-
 evalNativeFunction(Environment, Function, Arguments) ->
     EvaluatedValues = lists:map(fun(X) -> evalWithEnvironment(Environment, X) end, Arguments),
     FindAnError =
@@ -126,39 +133,95 @@ evalApplication(Environment, Abstraction, Arguments) ->
                            Abstraction)}
     end.
 
-genNativeFunc(Operator) ->
+genNativeArithmeticFunc(ArithmeticOperator) ->
     Function =
         fun(Element, Accum) ->
            case {Accum, Element} of
-               {{ok, {literal, Acc}}, {ok, {literal, X}}} ->
-                   {ok, {literal, Operator(Acc, X)}};
+               {{ok, {literal, {integer, Acc}}}, {ok, {literal, {integer, X}}}} ->
+                   {ok, {literal, {integer, ArithmeticOperator(Acc, X)}}};
                _ ->
-                   {error, "Could not operate on this"}
+                   {error, "Could not operate on this since it's not a number"}
            end
         end,
     {nativeFunc,
      fun(List) ->
         [Head | Tail] = List,
-        io:fwrite("~p~n", [Tail]),
         lists:foldl(Function, Head, Tail)
      end}.
 
-add() ->
-    {opAdd, genNativeFunc(fun erlang:'+'/2)}.
+genNativeComparisonFunc(Operator) ->
+    {nativeFunc,
+        fun([{ok, {literal, {integer, Left}}}, {ok, {literal, {integer, Right}}}]) ->
+            {ok, {literal, {boolean, Operator(Left, Right)}}};
+           (_) -> erlang:error("Could not eval valid comparison expression with arity 2")
+	    end}.
 
-subtract() ->
-    {opSub, genNativeFunc(fun erlang:'-'/2)}.
+genNativeLogicalFunc(BoolOperator, MEmpty) ->
+    Function =
+        fun(Element, Accum) ->
+            case {Accum, Element} of
+                {{ok, {literal, {boolean, Acc}}}, {ok, {literal, {boolean, X}}}} ->
+                    {ok, {literal, {boolean, BoolOperator(Acc, X)}}};
+                _ ->
+                    {error, "Could not operate on this since it's not a boolean"}
+            end
+        end,
+    {nativeFunc,
+        fun(List) ->
+            lists:foldl(Function, {ok, {literal, {boolean, MEmpty}}}, List)
+        end}.
 
-multiplication() ->
-    {opMul, genNativeFunc(fun erlang:'*'/2)}.
+boolLessThan() ->
+    {'<', genNativeComparisonFunc(fun erlang:'<'/2)}.
 
-division() ->
-    {opDiv,
-     genNativeFunc(fun (_, 0) ->
+boolGreaterThan() ->
+    {'>', genNativeComparisonFunc(fun erlang:'>'/2)}.
+
+boolEqual() ->
+    {'=', genNativeComparisonFunc(fun erlang:'='/2)}.
+
+boolGreaterEqualThan() ->
+    {'>=', genNativeComparisonFunc(fun erlang:'>='/2)}.
+
+boolLessEqualThan() ->
+    {'<=', genNativeComparisonFunc(fun erlang:'=<'/2)}.
+
+boolAnd() ->
+    {'&&', genNativeLogicalFunc(fun erlang:'and'/2, true)}.
+    
+boolOr() ->
+    {'||', genNativeLogicalFunc(fun erlang:'or'/2, false)}.
+
+arithAdd() ->
+    {'+', genNativeArithmeticFunc(fun erlang:'+'/2)}.
+
+arithSubtract() ->
+    {'-', genNativeArithmeticFunc(fun erlang:'-'/2)}.
+
+arithMultiplication() ->
+    {'*', genNativeArithmeticFunc(fun erlang:'*'/2)}.
+
+arithDivision() ->
+    {'/',
+    genNativeArithmeticFunc(fun (_, 0) ->
                            erlang:error("Division by zero is not allowed");
                        (A, B) ->
                            A / B
-                   end)}.
+                   end)}.                
+
+
+prelude() ->
+    maps:from_list([arithAdd(), 
+                    arithSubtract(), 
+                    arithMultiplication(), 
+                    arithDivision(), 
+                    boolAnd(), 
+                    boolOr(), 
+                    boolGreaterEqualThan(), 
+                    boolLessEqualThan(), 
+                    boolEqual(),
+                    boolLessThan(),
+                    boolGreaterThan()]).
 
 eval() ->
     %%Expression =
@@ -169,9 +232,17 @@ eval() ->
     %%    {application,
     %%    {abstraction, {variadic, [{rest, b}]}, {variable, b}},
     %%     [{literal, 1}, {literal, 2}, {literal, 3}]},
-    Expression =
+    ArithmeticExpr1 =
         {application,
-         {variable, opDiv},
-         [{literal, 1}, {literal, 2}, {literal, 2}, {literal, 2}]},
-    evalWithEnvironment(prelude(),
-                        Expression).    %%evalWithEnvironment(maps:new(), Expression).
+         {variable, '/'},
+         [{literal, {integer, 1}}, {literal, {integer, 2}}, {literal, {integer, 2}}, {literal, {integer, 2}}]},
+    ArithmeticExpr2 =
+        {application,
+         {variable, '*'},
+         [{literal, {integer, 1}}, {literal, {integer, 2}}, {literal, {integer, 2}}, {literal, {integer, 2}}]},
+    %% Expression =
+    %%     {condition, {application, {variable, opOr}, [{literal, {boolean, true}}, {literal, {boolean, false}}]}, ArithmeticExpr1, ArithmeticExpr2},
+    Expression =
+        {application, {variable, '<'}, [{literal, {integer, 1}}, {literal, {integer, 1}}]},
+
+    evalWithEnvironment(prelude(), Expression).    %%evalWithEnvironment(maps:new(), Expression).
